dffcc54e4e2ad7320330eea9fabb77607ec27a72
duncanchiu409 <chiuduncan409@gmail.com>
2025-08-14 15:19:31 +0800

    Add: first publish

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 // import { join } from 'node:path';
 // import { readFile } from 'node:fs/promises';
 // import OllamaService from './src/service/ollamaService';
-import { ArgumentParser } from "argparse";
+import NpmService from "./src/service/npmService";
 
 (async () => {
   // console.log("");
@@ -29,29 +29,32 @@ import { ArgumentParser } from "argparse";
   // console.log("---------- End of Huskit Commit Handler -----------");
   // console.log("");
 
-  const parser = new ArgumentParser({
-    description: "A simple CLI tool to generate commit messages",
-  });
+  // const parser = new ArgumentParser({
+  //   description: "A simple CLI tool to generate commit messages",
+  // });
 
-  const sum = (ints: number[]) => ints.reduce((a, b) => a + b, 0);
-  const max = (ints: number[]) =>
-    ints.reduce((a, b) => Math.max(a, b), -Infinity);
+  // const sum = (ints: number[]) => ints.reduce((a, b) => a + b, 0);
+  // const max = (ints: number[]) =>
+  //   ints.reduce((a, b) => Math.max(a, b), -Infinity);
 
-  parser.add_argument("integers", {
-    metavar: "N",
-    type: "int",
-    nargs: "+",
-    help: "an integer for the accumulator",
-  });
+  // parser.add_argument("integers", {
+  //   metavar: "N",
+  //   type: "int",
+  //   nargs: "+",
+  //   help: "an integer for the accumulator",
+  // });
 
-  parser.add_argument("--sum", {
-    dest: "accumulate",
-    action: "store_const",
-    const: sum,
-    default: max,
-    help: "sum the integers (default: find the max)",
-  });
+  // parser.add_argument("--sum", {
+  //   dest: "accumulate",
+  //   action: "store_const",
+  //   const: sum,
+  //   default: max,
+  //   help: "sum the integers (default: find the max)",
+  // });
 
-  const args = parser.parse_args();
-  console.log(args.accumulate(args.integers));
+  // const args = parser.parse_args();
+  // console.log(args.accumulate(args.integers));
+
+  const latestVersion = await NpmService.get_latest_package_version();
+  console.log(latestVersion);
 })();
--- src/service/npmService.ts
+++ src/service/npmService.ts
@@ -4,6 +4,14 @@ class NpmService {
   static async get_package_version() {
     return packageInfo.version as string;
   }
+
+  static async get_latest_package_version() {
+    const response = await fetch(
+      `https://registry.npmjs.org/${packageInfo.name}`,
+    );
+    const data = await response.json();
+    return data.version as string;
+  }
 }
 
 export default NpmService;
